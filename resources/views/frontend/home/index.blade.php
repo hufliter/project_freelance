2e59a32751dc74ff2bf2a999a62e392b4bad8102
tp6 <finalfantasy812@gmail.com>
2015-12-22 01:01:27 +0700

    Tuongthang.Phan

--- resources/views/frontend/home/index.blade.php
+++ resources/views/frontend/home/index.blade.php
@@ -1,5 +1,51 @@
 @extends('frontend.layout.index')
 @section('content')
+<div class="header-wrap">
+  <!-- HEADER -->
+  <header id="header-main">
+    <div class="container">
+      <div class="navbar yamm navbar-default">
+        <div class="navbar-header">
+          <button type="button" data-toggle="collapse" data-target="#navbar-collapse-1" class="navbar-toggle">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          </button>
+          <a href="/" class="navbar-brand"><img src="{{asset('img/logo.png')}}" width="40" style="width: 40px; height: 30px;" alt="Not Found"/></a>
+        </div>
+
+        <!-- SEARCH -->
+        <div class="header-x pull-right">
+          <div class="s-search">
+            <div class="ss-trigger"><i class="icon-search2"></i></div>
+            <div class="ss-content">
+              <span class="ss-close icon-cross2"></span>
+              <div class="ssc-inner">
+                <form>
+                  <input type="text" placeholder="Type Search text here...">
+                  <button type="submit"><i class="icon-search2"></i></button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div id="navbar-collapse-1" class="navbar-collapse collapse navbar-right">
+          <ul class="nav navbar-nav">
+            <li class="page-scroll"><a href="#page-top">Trang Chủ</a></li>
+            <li class="page-scroll"><a href="#1">Giới Thiệu</a></li>
+            <li class="page-scroll"><a href="#3">Sản Phẩm</a></li>
+            <li class="page-scroll"><a href="#2">Hỗ trợ kĩ thuật</a></li>
+            <!-- <li class="page-scroll"><a href="#4">Tỉ Giá & Thời Tiết</a></li> -->
+            <!-- <li class="page-scroll"><a href="#6">Kĩ Thuật</a></li> -->
+                        <li class="page-scroll"><a href="#5">Tin Tức</a></li>
+            <li class="page-scroll"><a href="#7">Liên Hệ</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </header>
+</div>
   <!-- SLIDER -->
   <div class="slider-wrap">
     <div class="tp-banner-container">
